c0837bde6175af193186f1e01da60a8a5f9f7907
Matthew Weier O'Phinney <matthew@zend.com>
2015-09-14 14:48:16 -0500

    Use null as default value for $options
    - FactoryInterface does this already; InvokableFactory should as well.

--- src/Factory/InvokableFactory.php
+++ src/Factory/InvokableFactory.php
@@ -19,7 +19,7 @@ final class InvokableFactory implements FactoryInterface
     /**
      * {@inheritDoc}
      */
-    public function __invoke(ContainerInterface $container, $requestedName, array $options = [])
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
     {
         return new $requestedName($options);
     }
