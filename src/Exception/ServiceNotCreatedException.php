a1734afc718c25c7c155c6995374f6ca07953b0e
Matthew Weier O'Phinney <matthew@zend.com>
2015-09-18 08:28:25 -0500

    Wrap long lines in class docblock

--- src/Exception/ServiceNotCreatedException.php
+++ src/Exception/ServiceNotCreatedException.php
@@ -13,7 +13,8 @@ use Interop\Container\Exception\ContainerException;
 use RuntimeException as SplRuntimeException;
 
 /**
- * This exception is thrown when the service locator do not manage to create the service (factory that has an error...)
+ * This exception is thrown when the service locator do not manage to create
+ * the service (factory that has an error...)
  */
 class ServiceNotCreatedException extends SplRuntimeException implements
     ContainerException,
