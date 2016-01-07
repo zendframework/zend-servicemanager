c0655c06ba9e446f20e7c8d3ebffd39995edb963
Matthew Weier O'Phinney <matthew@zend.com>
2016-01-07 13:58:04 -0600

    Adds the InvokableFactory
    Most of the component refactors are complete, and will need to be re-done.
    However, one item that should likely be retained is usage of the `InvokableFactory`
    for invokable instances. This patch adds that factory to the v2 series,
    having it implement `Zend\ServiceManager\FactoryInterface`, but retaining the
    defined invokable method from v3. It raises an exception if the `$requestedName`
    is not provided, ensuring that any tests against the functionality are using
    a `ServiceManager` instance.

---
+++ test/Factory/InvokableFactoryTest.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace ZendTest\ServiceManager\Factory;
+
+use Interop\Container\ContainerInterface;
+use PHPUnit_Framework_TestCase as TestCase;
+use Zend\ServiceManager\Factory\InvokableFactory;
+use ZendTest\ServiceManager\TestAsset\InvokableObject;
+
+/**
+ * @covers \Zend\ServiceManager\Factory\InvokableFactory
+ */
+class InvokableFactoryTest extends TestCase
+{
+    public function testCanCreateObject()
+    {
+        $container = $this->getMock(ContainerInterface::class);
+        $factory   = new InvokableFactory();
+
+        $object = $factory($container, InvokableObject::class, ['foo' => 'bar']);
+
+        $this->assertInstanceOf(InvokableObject::class, $object);
+        $this->assertEquals(['foo' => 'bar'], $object->options);
+    }
+}
