e9500b59417e9f5c391bff9acb23c2163562bcc6
Rob Allen <rob@akrabat.com>
2016-01-08 10:14:11 +0000

    Update docblock to relect rename to canCreate()

--- src/Factory/AbstractFactoryInterface.php
+++ src/Factory/AbstractFactoryInterface.php
@@ -15,11 +15,11 @@ use Interop\Container\ContainerInterface;
  * Interface for an abstract factory.
  *
  * An abstract factory extends the factory interface, but also has an
- * additional "has" method, which is called to check if the abstract factory
- * has the ability to create an instance for the given service. You should
- * limit the number of abstract factories to ensure good performance. Starting
- * from ServiceManager v3, remember that you can also attach multiple names to
- * the same factory, which reduces the need for abstract factories.
+ * additional "canCreate" method, which is called to check if the abstract
+ * factory has the ability to create an instance for the given service. You
+ * should limit the number of abstract factories to ensure good performance.
+ * Starting from ServiceManager v3, remember that you can also attach multiple
+ * names to the same factory, which reduces the need for abstract factories.
  */
 interface AbstractFactoryInterface extends FactoryInterface
 {
