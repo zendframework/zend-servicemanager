b8aa4747321de599ce12a5505352b52201854d61
Matthew Weier O'Phinney <matthew@zend.com>
2016-02-01 16:26:33 -0600

    Removed typechecking from setCreationOptions()
    The method typehints against `array` already, which is part of the
    `MutableCreationOptionsInterface` definition; there's no need to check
    inside the method body.

--- src/Factory/InvokableFactory.php
+++ src/Factory/InvokableFactory.php
@@ -109,18 +109,6 @@ final class InvokableFactory implements FactoryInterface
      */
     public function setCreationOptions(array $creationOptions)
     {
-        if ($creationOptions instanceof Traversable) {
-            $creationOptions = iterator_to_array($creationOptions);
-        }
-
-        if (! is_array($creationOptions)) {
-            throw new InvalidServiceException(sprintf(
-                '%s cannot use non-array, non-traversable creation options; received %s',
-                __CLASS__,
-                (is_object($creationOptions) ? get_class($creationOptions) : gettype($creationOptions))
-            ));
-        }
-
         $this->creationOptions = $creationOptions;
     }
 }
