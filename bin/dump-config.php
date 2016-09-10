cefe38f914d90f2027f349605ef7dff3b0333d71
Matthew Weier O'Phinney <matthew@zend.com>
2016-09-09 21:10:11 -0500

    Better error handling
    - use env to find php
    - do not throw exceptions; instead:
      - write to STDERR
      - exit 1
    - test for each of:
      - config file exists
      - config file returns an array
      - class name exists

--- bin/dump-config.php
+++ bin/dump-config.php
@@ -1,23 +1,41 @@
+#!/usr/bin/env php
 <?php
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
+ * @link      http://github.com/zendframework/zend-servicemanager for the canonical source repository
+ * @copyright Copyright (c) 2016 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 
-require_once(__DIR__ . '/../vendor/autoload.php');
-chdir(__DIR__ . '/../');
+namespace Zend\ServiceManager;
+
+// Setup/verify autoloading
+if (file_exists($a = __DIR__ . '/../../../autoload.php')) {
+    require $a;
+} elseif (file_exists($a = __DIR__ . '/../vendor/autoload.php')) {
+    require $a;
+} else {
+    fwrite(STDERR, 'Cannot locate autoloader; please run "composer install"' . PHP_EOL);
+    exit(1);
+}
 
 $configPath = isset($argv[1]) ? $argv[1] : '';
 $className = isset($argv[2]) ? $argv[2] : '';
 
 // Retrieve configuration
-if (!file_exists($configPath)) {
-    throw new InvalidArgumentException('Cannot find any config at ' . $configPath);
+if (! file_exists($configPath)) {
+    fwrite(STDERR, sprintf('Cannot find configuration file at path "%s"%s', $configPath, PHP_EOL));
+    exit(1);
 }
 
 $appConfig = require $configPath;
+if (! is_array($appConfig)) {
+    fwrite(STDERR, sprintf('Configuration file at path "%s" does not return an array%s', $configPath, PHP_EOL));
+    exit(1);
+}
 
-\Zend\ServiceManager\Tool\CliTool::createDependencyConfig($appConfig, $className);
+if (! class_exists($className)) {
+    fwrite(STDERR, sprintf('Class "%s" does not exist%s', $className, PHP_EOL));
+    exit(1);
+}
+
+Tool\CliTool::createDependencyConfig($appConfig, $className);
