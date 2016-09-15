ebb1c00c9381e0bfbeebd2e854afa6a9f795e360
Matthew Weier O'Phinney <matthew@zend.com>
2016-09-15 16:36:27 -0500

    Consistent docblocks
    - Reference actual repository in `@link` annotation
    - Reference correct year sequence. If unknown, left at 2005-2016;
      otherwise, updated to known earliest date, and this year.

--- benchmarks/BenchAsset/AbstractFactoryFoo.php
+++ benchmarks/BenchAsset/AbstractFactoryFoo.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * @link      http://github.com/zendframework/zend-servicemanager for the canonical source repository
+ * @copyright Copyright (c) 2016 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
 namespace ZendBench\ServiceManager\BenchAsset;
 
 use Zend\ServiceManager\Factory\AbstractFactoryInterface;
@@ -18,5 +24,4 @@ class AbstractFactoryFoo implements AbstractFactoryInterface
     {
         return ($requestedName === 'foo');
     }
-
 }
