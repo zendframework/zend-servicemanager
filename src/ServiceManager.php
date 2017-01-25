354cdab29eb738e62d0a6d52ae1a2012790c1faf
Fabian Derschatta <derschatta@united-domains.de>
2017-01-25 12:04:42 +0100

    Implemented new method getShared to make recursive handling of shared flag possible, created test cases

--- src/ServiceManager.php
+++ src/ServiceManager.php
@@ -456,6 +456,53 @@ class ServiceManager implements ServiceLocatorInterface
     }
 
     /**
+     * returns null if shared value is not set at all otherwise it returns the value
+     *
+     * @param  string $name
+     * @param  bool   $usePeeringServiceManagers
+     * @return bool|null
+     * @throws Exception\ServiceNotFoundException
+     */
+    public function getShared($name, $usePeeringServiceManagers = true)
+    {
+        $cName = $this->canonicalizeName($name);
+
+        if (!$this->has($name)) {
+            throw new Exception\ServiceNotFoundException(sprintf(
+                '%s: A service by the name "%s" was not found',
+                get_class($this) . '::' . __FUNCTION__,
+                $name
+            ));
+        }
+
+        if (!isset($this->shared[$cName]) && $usePeeringServiceManagers) {
+            $caller = $this->serviceManagerCaller;
+            foreach ($this->peeringServiceManagers as $peeringServiceManager) {
+                // ignore peering service manager if they are the same instance
+                if ($caller === $peeringServiceManager) {
+                    continue;
+                }
+
+                $peeringServiceManager->serviceManagerCaller = $this;
+
+                $shared = $peeringServiceManager->getShared($cName);
+                if ($shared !== null) {
+                    $peeringServiceManager->serviceManagerCaller = null;
+                    return $shared;
+                }
+
+                $peeringServiceManager->serviceManagerCaller = null;
+            }
+        }
+
+        if (isset($this->shared[$cName])) {
+            return $this->shared[$cName];
+        }
+
+        return null;
+    }
+
+    /**
      * Resolve the alias for the given canonical name
      *
      * @param  string $cName The canonical name to resolve
@@ -1038,7 +1085,11 @@ class ServiceManager implements ServiceLocatorInterface
 
             if ($peeringServiceManager->has($name)) {
                 $cname = $this->canonicalizeName($name);
-                $this->shared[$cname] = $peeringServiceManager->isShared($name);
+                $shared = $peeringServiceManager->getShared($name);
+                if ($shared === null) {
+                    $shared = $this->shareByDefault();
+                }
+                $this->shared[$cname] = $shared;
                 $instance = $peeringServiceManager->get($name);
                 $peeringServiceManager->serviceManagerCaller = null;
                 return $instance;
