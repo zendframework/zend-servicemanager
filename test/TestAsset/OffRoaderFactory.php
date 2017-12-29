19f774a7a04c576479684bb2f9d80b2d3d614c86
fhein <frank.hein@maxence.de>
2017-12-29 08:06:05 +0100

    File set adjusted.

--- test/TestAsset/OffRoaderFactory.php
+++ test/TestAsset/OffRoaderFactory.php
@@ -5,12 +5,12 @@ namespace ZendTest\ServiceManager\TestAsset;
 use Zend\ServiceManager\Factory\FactoryInterface;
 use Interop\Container\ContainerInterface;
 
-class CarFactory implements FactoryInterface
+class OffRoaderFactory implements FactoryInterface
 {
     public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
     {
         $car = new Car();
-        $car->classifier = 'I was created by a car factory, no diesel issues, I promise';
+        $car->classifier = 'I am a factory produced offroader.';
         return $car;
     }
 }
