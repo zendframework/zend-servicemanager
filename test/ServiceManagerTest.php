d7641745258b082c336967626278b6365ee6af21
fhein <frank.hein@maxence.de>
2018-01-23 17:09:55 +0100

    Rest of requests.

--- test/ServiceManagerTest.php
+++ test/ServiceManagerTest.php
@@ -306,28 +306,4 @@ class ServiceManagerTest extends TestCase
         $serviceManager = new SimpleServiceManager($config);
         $this->assertEquals(stdClass::class, get_class($serviceManager->get(stdClass::class)));
     }
-
-//     public function testMinimalCyclicAliasDefinitionShouldThrow()
-//     {
-//         $sm = new ServiceManager();
-
-//         $this->expectException(CyclicAliasException::class);
-//         $sm->setAlias('alias', 'alias');
-//     }
-
-//     public function testCoverageDepthFirstTaggingOnRecursiveAliasDefinitions()
-//     {
-//         $sm = new ServiceManager([
-//             'factories' => [
-//                 stdClass::class => InvokableFactory::class,
-//             ],
-//             'aliases' => [
-//                 'alias1' => 'alias2',
-//                 'alias2' => 'alias3',
-//                 'alias3' => stdClass::class,
-//             ],
-//         ]);
-//         $this->assertSame($sm->get('alias1'), $sm->get('alias2'));
-//         $this->assertSame($sm->get(stdClass::class), $sm->get('alias1'));
-//     }
 }
