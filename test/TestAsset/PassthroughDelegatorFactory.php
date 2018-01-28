0e40bd21f324ccb69901b642685fdf9ab1d7c4fa
fhein <frank.hein@maxence.de>
2018-01-28 02:28:51 +0100

    Adds TaggingInitializer, AbstractFactoryFoo and PassthroughDelegator test assets.

--- test/TestAsset/PassthroughDelegatorFactory.php
+++ test/TestAsset/PassthroughDelegatorFactory.php
@@ -16,8 +16,12 @@ class PassthroughDelegatorFactory implements DelegatorFactoryInterface
      * {@inheritDoc}
      * @see \Zend\ServiceManager\Factory\DelegatorFactoryInterface::__invoke()
      */
-    public function __invoke(ContainerInterface $container, $name, callable $callback, array $options = null)
-    {
-        return call_user_func($callback);
+    public function __invoke(
+        \Interop\Container\ContainerInterface $container,
+        $name,
+        callable $callback,
+        array $options = null
+    ) {
+        return $options === null ? $callback($options) : $callback();
     }
 }
