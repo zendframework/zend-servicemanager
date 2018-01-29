5afab417476b90205049bcd2256dc2560213f63a
fhein <frank.hein@maxence.de>
2018-01-29 19:14:56 +0100

    Removed obsolete import. Switched to https and 2018.

--- test/ServiceManagerTest.php
+++ test/ServiceManagerTest.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * @link      http://github.com/zendframework/zend-servicemanager for the canonical source repository
- * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
+ * @link      https://github.com/zendframework/zend-servicemanager for the canonical source repository
+ * @copyright Copyright (c) 2005-2018 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 
@@ -17,7 +17,6 @@ use Zend\ServiceManager\Factory\InvokableFactory;
 use Zend\ServiceManager\ServiceManager;
 use ZendTest\ServiceManager\TestAsset\InvokableObject;
 use ZendTest\ServiceManager\TestAsset\SimpleServiceManager;
-use Zend\ServiceManager\Exception\CyclicAliasException;
 use PHPUnit\Framework\MockObject\Invokable;
 
 /**
